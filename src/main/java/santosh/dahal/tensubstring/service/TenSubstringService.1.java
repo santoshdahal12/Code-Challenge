550f083349939da249cddfcf1e27a93da75403eb
DESKTOP-EQ6GNNV\santosh dahal <santoshdahal2072@gmail.com>
2017-07-26 22:57:43 -0400

    first commit for feature branch

---
+++ src/main/java/santosh/dahal/tensubstring/service/TenSubstringService.1.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package santosh.dahal.tensubstring.service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author santosh dahal
+ *
+ */
+
+public class TenSubstringService {
+
+	public static sayHello(){
+		System.out.println("I am saying hellloooooo");
+	}
+
+}
